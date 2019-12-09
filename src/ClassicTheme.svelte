18303cb41ac2b9df245d5557151a56d73cfe4140
joefong <joemanfong@gmail.com>
2019-12-09 00:02:15 -0800

    update

---
+++ src/ClassicTheme.svelte
@@ -0,0 +1,324 @@
+<script>
+    export let firstName;
+    export let lastName;
+    export let email;
+    export let phoneNumber;
+    export let socialMedia;
+    export let professionalSummary;
+    export let experience;
+    export let keyProjects;
+    export let education;
+
+	export const resume = undefined;
+</script>
+
+<div id="resumeContainer" class="c14 c18">
+      <p class="c31"><span class="c24 c16 c27">{firstName}<span class="c24 c27 c16">&nbsp;{lastName}</span></p>
+      <p class="c1"><span class="c12 c34"><a class="c7" href="mailto:{email}">{email}</a></span><span class="c12">&nbsp;| </span><span class="c16 c12">{phoneNumber}</span><span class="c6 c4">&nbsp;</span></p>
+      <p class="c1">
+	  	{#each socialMedia as socialMediaLink, index}
+		<span class="c25 c14 c12">
+            <a class="c7" href="{socialMediaLink}">{socialMediaLink}</a>
+		</span>
+			{#if socialMedia.length != (index + 1)}
+			<span class="c12"> | </span>
+			{:else}
+			<span class="c6 c12 c32">&nbsp;</span>
+			{/if}
+		{/each}
+      </p>
+      <h1 class="c29"><span class="c24 c11 c16">PROFESSIONAL SUMMARY</span></h1>
+      <ul class="c23 lst-kix_list_14-0 start">
+	  	{#each professionalSummary as item, index}
+		<li class="c17 c5">
+            <span class="c14 c12">
+              {item.skill}
+			  {#each item.bolded as itemBolded, i}
+			  	<span class="c8 c4">{itemBolded}</span>
+			  	{#if item.bolded.length != (i + 1)}
+				<span class="c3 c4">, </span>
+				{:else}
+				<span class="c3 c4"></span>
+				{/if}
+			  {/each}
+            </span>
+          </li>
+		{/each}
+      </ul>
+      <h1 class="c19"><span class="c24 c11 c16">EXPERIENCE</span></h1>
+		{#each experience as item, index}
+			<p class="c1"><span class="c24 c4 c16">{item.jobTitle} | {item.role} | {item.duration}</span></p>
+			<p class="c1"><span class="c12 c37">{item.summaryOfCompany}</span></p>
+			<ul class="c23 lst-kix_list_2-0 start">
+			{#each item.bulletsOfRole as itemBullets, i}
+				<li class="c0"><span class="c6 c4">{itemBullets}</span></li>
+			{/each}
+			</ul>
+		{/each}
+      <h1 id="page-break-1" class="c19"><span class="c24 c11 c16">KEY PROJECTS</span></h1>
+	  	{#each keyProjects as item, index}
+			<p class="c10"><span class="c24 c4 c16">{item.role} (Client: {item.projectName})</span></p>
+        	<ul class="c23 lst-kix_list_2-0">
+			{#each item.bulletsOfRole as itemBullets, i}
+				<li class="c0"><span class="c6 c4">{itemBullets}</span></li>
+			{/each}
+			</ul>
+		{/each}
+      <h1 class="c19" id="h.aljsgaeh8u18"><span class="c24 c11 c16">EDUCATION</span></h1>
+	  	{#each education as item, index}
+			<p class="c1"><span>{item.schoolName} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.yearCompleted}<br>{item.educationCompleted}</span></p>
+		{/each}
+      <div>
+          <p class="c33 c35"><span class="c6 c11"></span></p>
+      </div>
+    </div>
+
+<style>
+  
+  .c29 {
+      padding-top: 14pt;
+      border-top-width: 3pt;
+      border-bottom-color: #7f7f7f;
+      padding-bottom: 5pt;
+      line-height: 1.0;
+      page-break-after: avoid;
+      border-top-style: solid;
+      border-bottom-width: 1pt;
+      border-top-color: #262626;
+      border-bottom-style: solid;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+  
+  .c19 {
+      padding-top: 5pt;
+      border-top-width: 3pt;
+      border-bottom-color: #7f7f7f;
+      padding-bottom: 5pt;
+      line-height: 1.0;
+      page-break-after: avoid;
+      border-top-style: solid;
+      border-bottom-width: 1pt;
+      border-top-color: #262626;
+      border-bottom-style: solid;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+  
+  .c0 {
+      margin-left: 18pt;
+      padding-top: 0pt;
+      padding-left: -7.2pt;
+      padding-bottom: 6pt;
+      line-height: 1.3;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+  
+  .c10 {
+      margin-left: 10.8pt;
+      padding-top: 0pt;
+      text-indent: -10.8pt;
+      padding-bottom: 6pt;
+      line-height: 1.3;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+  
+  .c22 {
+      padding-top: 0pt;
+      text-indent: -10.5pt;
+      padding-bottom: 6pt;
+      line-height: 1.3;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+  
+  .c31 {
+      padding-top: 0pt;
+      padding-bottom: 9pt;
+      line-height: 1.0;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+
+  .c8 {
+    font-weight: 700;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-family: "Tahoma";
+    font-style: normal
+}
+  
+  .c3 {
+      font-weight: 400;
+      text-decoration: none;
+      vertical-align: baseline;
+      font-family: "Tahoma";
+      font-style: normal
+  }
+  
+  .c35 {
+      padding-top: 12pt;
+      padding-bottom: 0pt;
+      line-height: 1.0;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+  
+  .c17 {
+      padding-top: 0pt;
+      padding-bottom: 0pt;
+      line-height: 1.3;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+  
+  .c1 {
+      padding-top: 0pt;
+      padding-bottom: 9pt;
+      line-height: 1.3;
+      orphans: 2;
+      widows: 2;
+      text-align: left
+  }
+
+.c6 {
+    font-weight: 400;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-family: "Tahoma";
+    font-style: normal
+}
+
+.c25 {
+    text-decoration-skip-ink: none;
+    -webkit-text-decoration-skip: none;
+    color: #1155cc;
+    text-decoration: underline
+}
+
+.c34 {
+    text-decoration-skip-ink: none;
+    -webkit-text-decoration-skip: none;
+    color: #53c3c7;
+    text-decoration: underline
+}
+
+.c24 {
+    text-decoration: none;
+    vertical-align: baseline;
+    font-family: "Tahoma";
+    font-style: normal
+}
+
+.c7 {
+    color: inherit;
+    text-decoration: inherit
+}
+
+
+.c18 {
+    max-width: 500.2pt;
+    padding: 44.8pt 33.5pt 52pt 33.5pt
+}
+
+.c23 {
+    padding: 0;
+    margin: 0
+}
+
+.c5 {
+    margin-left: 20pt;
+    padding-left: 0pt
+}
+
+.c4 {
+    color: #7f7f7f;
+    font-size: 9pt
+}
+
+.c11 {
+    color: #0e0b05;
+    font-size: 12pt
+}
+
+.c27 {
+    color: #0e0b05;
+    font-size: 30pt
+}
+
+.c33 {
+    height: 10pt
+}
+
+.c37 {
+    font-style: italic
+}
+
+.c16 {
+    font-weight: 700
+}
+
+.c32 {
+    color: #000000
+}
+
+.c12 {
+    font-size: 9pt
+}
+
+li {
+    color: #7f7f7f;
+    font-size: 10pt;
+    font-family: "Tahoma"
+}
+
+p {
+    margin: 0;
+    color: #7f7f7f;
+    font-size: 10pt;
+    font-family: "Tahoma"
+}
+
+h1 {
+    padding-top: 5pt;
+    color: #0e0b05;
+    border-top-width: 3pt;
+    border-bottom-color: #7f7f7f;
+    font-weight: 700;
+    font-size: 12pt;
+    padding-bottom: 5pt;
+    line-height: 1.0;
+    page-break-after: avoid;
+    border-top-style: solid;
+    border-bottom-width: 1pt;
+    border-top-color: #262626;
+    font-family: "Tahoma";
+    border-bottom-style: solid;
+    orphans: 2;
+    widows: 2;
+    text-align: left
+}
+
+@media print {
+    #page-break-1 {
+        page-break-before: always;
+    }
+  }
+
+@media only screen and (max-width: 600px) {
+    .c18 {
+        max-width: 500.2pt;
+        padding: 54.8pt 43.2pt 62pt 48.5pt
+    }
+}
+</style>
---
+++ src/Resume.svelte
@@ -0,0 +1,144 @@
+<script>
+    import ClassicTheme from './ClassicTheme.svelte';
+    
+    export const resume = {
+    "firstName": "Joeman",
+    "lastName": "Fong",
+    "email": "joemanfong@gmail.com",
+    "phoneNumber": "778.883.5667",
+    "socialMedia": [
+        "https://linkedin.com/in/joemanfong",
+        "https://github.com/joeFong",
+        "https://medium.com/@joemanfong",
+        "https://joefong.xyz"
+    ],
+    "professionalSummary": [
+        {
+        "skill": "3+ years of experience in full-stack web application development",
+        "bolded": []
+        },
+        {
+        "skill": "3+ years of experience working with web technologies including;",
+        "bolded": ["PHP", "JavaScript", "NodeJS"]
+        },
+        {
+        "skill": "2+ years building and architecturing marketplace solutions with",
+        "bolded": ["Stripe"]
+        },
+        {
+        "skill": "2+ years deploying and writing automated builds with",
+        "bolded": ["Gulp", "Webpack"]
+        },
+        {
+        "skill": "2+ years providing SEO solutions with",
+        "bolded": ["Google Analytics", "Google Tag Manager",  "Facebook Analytics"]
+        },
+        {
+        "skill": "2+ years of experience working with relational databases including;",
+        "bolded": ["MySQL", "SQL Server"]
+        },
+        {
+        "skill": "2+ years of experience working with open-source libraries and frameworks including;",
+        "bolded": ["Laravel", "Drupal", "Wordpress", "Socket.IO", "Web Driver.IO", "Jest", "AngularJS", "Angular 2-4", "React", "React Native", "Redux", "Polymer", "LitHTML", "VueJS"]
+        },
+        {
+        "skill": "2+ years of experience operating in an agile development environment in a ",
+        "bolded": ["LAMP", "LEMP", "stack"]
+        },
+        {
+        "skill": "Working as a DevOps Engineer with experience in a Cloud environment using various technologies such as AWS EC2, Dockers, Containers, Vultr, Iocage, and Nginx Reverse Proxies on a ",
+        "bolded": ["Linux OS"]
+        }
+    ],
+    "experience": [
+        {
+        "jobTitle": "Devradius",
+        "role": "Full Stack Developer",
+        "duration": "April 2017 - Present",
+        "summaryOfCompany": "Development agency focused on providing website and application development, CMS development and dedicated and shared hosting cloud environment solutions to businesses.",
+        "bulletsOfRole": [
+            "Design, architect and implement complex SaaS, web, android and iOS applications for key clients",
+            "Lead weekly agile sprints and participate in weekly team meetings to review project pipelines",
+            "Mentor junior co-op students and developers through shadowing and overseeing project responsibilities"
+        ]
+        },
+        {
+        "jobTitle": "Thrinacia",
+        "role": "Front End Developer",
+        "duration": "April 2017 - Present (Same organization as Devradius)",
+        "summaryOfCompany": "Providing SaaS crowdfunding infrastructure services focused on building, customizing and deploying white label crowdfunding applications/platforms, campaigns, widgets and applications to customers.",
+        "bulletsOfRole": [
+            "Lead the flagship software product through designing, developing and maintaining front-end software development features",
+            "Walk clients through project scope and deliverables and provide ongoing client consultation",
+            "Contributed to the deployment of over 6000+ SaaS crowdfunding platforms"
+        ]
+        },
+        {
+        "jobTitle": "DRIVE Business Intelligence Inc., Quupe Technologies (Internship Experience)",
+        "role": "Full-Stack Developer",
+        "duration": "September 2016 – May 2017",
+        "summaryOfCompany": "",
+        "bulletsOfRole": []
+        }
+    ],
+    "keyProjects": [
+        {
+        "projectName": "Spoonful",
+        "role": "Full Stack Developer",
+        "bulletsOfRole": [
+            "Design, architect, implement native Android and IOS application with React Native",
+            "Design and develop a RESTful API with Headless Wordpress",
+            "Implement OAuth2 Authentication for Social Logins",
+            "Create modular plugins for data synchronization of over 300K and growing identified scannable products in the USA"
+        ]
+        },
+        {
+        "projectName": "PHM",
+        "role": "Full Stack Developer",
+        "bulletsOfRole": [
+            "Design, architect, implement a web application for a technical recruitment company",
+            "Scope and implement an internal system for managing email campaigns for clients/users",
+            "Functions include candidate registration, client application, and viewable jobs via filterable queries"
+        ]
+        },
+        {
+        "projectName": "Aventria",
+        "role": "Full Stack Developer",
+        "bulletsOfRole": [
+            "Design, architect, implement complex web applications to quickly deploy customized user plugins containing a suite of tools for over 200+ health clinics and hospitals all across the US",
+            "Develop scalable solutions from front to back end for major pharmaceutical companies to quickly diagnose patients",
+            "Maintain and extend functionality for all sites in production"
+        ]
+        },
+        {
+        "projectName": "StudySquare",
+        "role": "Full Stack Developer",
+        "bulletsOfRole": [
+            "Develop a complex peer-to-peer tutoring web application with Stripe marketplace architecture",
+            "Design and develop a white boarding tool with sockiet.IO for users and tutors to concurrently connect",
+            "Implement a chat system for users to communicate concurrently",
+            "Implement Stripe API to enable a marketplace platform"
+        ]
+        },
+        {
+        "projectName": "Thrinacia",
+        "role": "Full Stack Developer",
+        "bulletsOfRole": [
+            "Implement front end business logic for Stripe marketplace including PCI compliant Stripe Elements, and Strong Customer Authentication",
+            "Strong understanding of marketplace solutions",
+            "Architect, design and develop embeddable crowdfunding tools in React, LitHTML, and Polymer",
+            "Manage, maintain and develop an embeddable complex web components developed in AngularJs and Angular"
+        ]
+        }
+    ],
+    "education": [
+            {
+            "schoolName": "British Columbia Institute of Technology",
+            "yearCompleted": "2017",
+            "educationCompleted": "Associates Degree, Graduated With Distinctions, Computer Technology/Computer Systems Technology"
+            }
+        ]
+}
+</script>
+
+<ClassicTheme {...resume}/>
